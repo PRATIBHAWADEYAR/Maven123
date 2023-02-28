c4318262f9b4dbbf551e850cd77176e29cb7bf4c
PRATIBHAWADEYAR <wadeyarpratibha@gmail.com>
2023-02-28 12:57:50 +0530

    First commit.

---
+++ Maven123/src/main/java/build/Mavendemo1.java
@@ -0,0 +1,8 @@
+package build;
+
+public class Mavendemo1 {
+	public String SamMethod() {
+		return "SamMethod";
+	}
+
+}
---
+++ Maven123/src/test/java/build/SamMethodTest.java
@@ -0,0 +1,14 @@
+package build;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SamMethodTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
